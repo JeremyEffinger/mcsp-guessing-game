8dbd060d75535bcd0f543b4fc32545d16e42c1e1
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-11-07 15:31:45 -0600

    Added Guess once, Guess again, how many tries, history, guess who and play again.

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 play(); // This is your main function that runs when the page loads
 
-function play(){
-   
+function play() {
+  let tries = 1;
+  let keepPlaying = true;
+  let number = 100;
+  let GenerateNumber = () => Math.floor(Math.random() * number);
+  let secretNumber = GenerateNumber();
+  let playerGuesses = [];
+  let playerName = prompt("Input your name!");
+  while (keepPlaying) {
+    let playerGuess = parseInt(prompt("What is your guess?"));
+    playerGuesses.push(playerGuess);
+    tries++;
+    switch (true) {
+      case playerGuess === secretNumber:
+        alert(
+          `Correct ${playerName}! Your previous guesses we're ${playerGuesses.join(
+            ", "
+          )}.`
+        );
+        alert(`It only took you ${tries} tries.`);
+        let keepPlayingQuestion = prompt(
+          `keep playing ${playerName}?`
+        ).toLocaleLowerCase();
+        if (keepPlayingQuestion === "y" || keepPlayingQuestion === "yes") {
+          secretNumber = GenerateNumber();
+          tries = 1;
+          playerGuesses = [];
+          continue;
+        } else {
+          keepPlaying = false;
+        }
+        break;
+      case playerGuess > secretNumber:
+        alert("Sorry bro too high.");
+        break;
+      case playerGuess < secretNumber:
+        alert("Sorry bro too low");
+        break;
+      default:
+        alert("Sorry man try again");
+        break;
+    }
+  }
 }
 
-function playAgain(){
-    
-}
+function playAgain() {}
